4198b94c133d9c251b1b7ea9f82bb7153189f231
Jonathan Slack <tun40549@temple.edu>
2022-10-06 15:32:50 -0400

    placeholders for navbar, register

--- FashionFinderDjango/FashionFinder/fashionfinderapp/urls.py
+++ FashionFinderDjango/FashionFinder/fashionfinderapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import include
+from django.contrib import admin
 
 # Look at views.py index() function and get that path
 urlpatterns = [
@@ -9,4 +11,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('pieces', views.pieces, name='pieces'),
     path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', views.register),
+
 ]

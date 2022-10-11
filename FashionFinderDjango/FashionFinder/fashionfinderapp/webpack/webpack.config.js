e02546cdafba5886a8f19f80bb773d94279b4145
Jonathan Slack <tun40549@temple.edu>
2022-10-11 16:59:55 -0400

    webpack config for react registration/ navbar

---
+++ FashionFinderDjango/FashionFinder/fashionfinderapp/webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+      register: path.resolve('jsx/register.jsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: [{loader: "babel-loader", options: {
+          "presets": [
+            "@babel/preset-env",
+            "@babel/preset-react"
+          ],
+          "plugins": [
+            ["@babel/plugin-transform-react-jsx", { "pragma":"React.createElement", "pragmaFrag":"React.Fragment" }],
+            ["@babel/plugin-proposal-class-properties", { "loose": true }],
+            ["@babel/plugin-proposal-private-methods", { "loose": true }],
+            ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
+            "@babel/plugin-proposal-optional-chaining"
+          ]
+        }}]
+      }
+    ]
+  },
+  plugins: [new ESLintPlugin({})],
+  resolve: {
+    alias: {
+      jsx: path.resolve('jsx/'),
+    },
+    extensions: [".js", ".jsx", ".json"],
+    mainFields: ["browser", "module", "main", "style"]
+  },
+};

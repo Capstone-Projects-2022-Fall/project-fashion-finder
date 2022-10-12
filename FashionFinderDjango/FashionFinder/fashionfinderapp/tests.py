3c8356819869d40c65fd818d7b5efbe83ca655ef
Jonathan Slack <tun40549@temple.edu>
2022-10-11 23:11:28 -0400

    basic tests

--- FashionFinderDjango/FashionFinder/fashionfinderapp/tests.py
+++ FashionFinderDjango/FashionFinder/fashionfinderapp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
-
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class Tests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        u = User.objects.create_user('foobar', 'foo@bar.com', 'password')
+        u.first_name = "Foo"
+        u.last_name = "Bar"
+        u.save()
+        self.User = u
+
+
+    def test_FF(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_get_user(self):
+        us = User.objects.filter(id=self.User.id).first()
+        self.assertNotEqual(us, None)
+        self.assertEqual(us.id, self.User.id)

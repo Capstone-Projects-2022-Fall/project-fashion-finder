36cc5fd091e3d04f5b1cc51e06341a25bc34a960
Mary Kate Durnan <marykated10@gmail.com>
2022-11-17 16:21:26 -0500

    Remove verson contro of generated fs

--- FashionFinderDjango/FashionFinder/fashionfinderapp/static/jsx/wardrobe.jsx
+++ FashionFinderDjango/FashionFinder/fashionfinderapp/static/jsx/wardrobe.jsx
@@ -28,6 +28,64 @@ const CSRFToken = () => {
 
 // }
 
+const WardrobeStyle = () => {
+	return <style>
+		{`
+			.wardrobe-item{
+				display: flex;
+			}
+			.wardrobe-items-container{
+				margin-left: 10%;
+				margin-right: 10%;
+				min-width: 400px;
+			}
+			.wardrobe-item-image{
+				border: 1px solid black;
+				margin-top: 5px;
+				margin-bottom: 5px;
+			}
+			.wardrobe-item-description-container{
+				margin-left: 100px;
+				margin-top: 10px;
+				border: 1px solid black;
+				display: inline;
+				max-width: 400px;
+				min-width: 300px;
+			}
+			.wardrobe-item-descriptor {
+				font-size: 50px;
+			}
+			.wardrobe-item-owner {
+				font-size: 30px;
+			}
+			.wardrobe-label-header {
+				font-weight: bold;
+				font-size: 30px;
+			}
+			.wardrobe-item-hex-codes-header{
+				font-weight: bold;
+				font-size: 30px;
+			}
+			.wardrobe-item-label-item {
+				font-size: 20px;
+			}
+			.box {
+				float: left;
+				height: 20px;
+				width: 20px;
+				margin-bottom: 15px;
+				border: 1px solid black;
+				clear: both;
+			  }
+			  
+			.wardrobe-item-hex-list-item {
+				display: inline-table;
+			}
+			
+		`}
+	</style>
+}
+
 
 class Wardrobe extends React.Component {
 	constructor(props) {
@@ -45,6 +103,62 @@ class Wardrobe extends React.Component {
 		.then(json => this.setState({loading: false, items: json}))
 	}
 
+	renderWardrobeItem(item, index){
+		if(item == null || item == undefined){
+			return <p> Item can not be properly rendered</p>
+		}
+
+		return <div className='wardrobe-item' key={index}>
+
+			<img className='wardrobe-item-image' src={item.filepath}
+				width="300"
+				height="400"
+			/>
+			<div className='wardrobe-item-description-container'>
+				<p className='wardrobe-item-descriptor'> {item.descriptor}</p>
+				<p className='wardrobe-item-owner'> Owner: {item.user_django_name} </p>
+				<div className='wardrobe-item-labels'>
+					<div className='wardrobe-label-header'>
+						Labels
+					</div>
+
+					<ul>
+						{item.labels.map((item,index) => <li className='wardrobe-item-label-item' key={index}>{item} </li> )}
+					</ul>
+				</div>
+				<div className='wardrobe-item-hex-codes'>
+					<p className='wardrobe-item-hex-codes-header'> Detected Colors: </p>
+					<ul>
+						{item.hex_codes.map((hex_code, index) => 
+							
+							<li className='wardrobe-item-hex-list-item'key={index}>
+								<div className='box' style={{backgroundColor: "#" + hex_code}}> </div>
+								{'#' + hex_code}
+							</li>
+						
+						)};
+					</ul>
+					{/* <ul> */}
+					{/* {item.hex_codes.map(item=> <div className='box' style={{background-color: '#348898'}}> </div>)} */}
+					{/* </ul> */}
+				</div>
+				<div className='wardrobe-item-links'>
+					<div className='wardrobe-link-see-full-image'>
+						<a href={'/static/' + item._id + '.jpg'}> Click here to see the full image</a>
+						<br/>
+						<br/>
+						<br/>
+						<a href={'/recommendations/' + item._id}> Click here to see images like this one</a>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+
+	}
+
+
 	renderList(items) {
 		console.log(Object.keys(items))
 		console.log(typeof(items))
@@ -56,13 +170,9 @@ class Wardrobe extends React.Component {
 		} 
 
 		return <ul>
-			{items.recs.map(item => (
-				<li>
-					{item.descriptor}
-				</li>
-			)
+			{items.recs.map((item,index) => this.renderWardrobeItem(item, index))
 			
-			)}
+			}
 
 		</ul>
 	}
@@ -80,6 +190,7 @@ class Wardrobe extends React.Component {
 		const {loading, items} = this.state
         return (
 			<>
+				<WardrobeStyle/>
 				<div className="wardrobe-items-container">
 					{loading ? "Loading...": this.renderList(this.state.items)}
 				</div>
